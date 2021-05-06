94df4ea1cffe8e4e1be775572304c7179681e11f
Mayank Sharma <66236773+mayank8june@users.noreply.github.com>
2021-05-06 06:39:32 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,122 @@
+import 'package:orignal/model/Days_Setting.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String title = '100 Days Of Sweat';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(primarySwatch: Colors.deepOrange),
+    home: MainPage(title: title),
+  );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final daysCompleted = DaysSetting(title: 'Challange Completed');
+  final notifications = List<DaysSetting>.generate(100, (i) => DaysSetting(title: 'Day${i + 1}'));
+
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text(widget.title),
+    ),
+    body: ListView(
+      children: [
+        buildToggleCheckbox(daysCompleted),
+        Divider(),
+        ...notifications.map(buildSingleCheckbox).toList(),
+      ],
+    ),
+  );
+
+  Widget buildToggleCheckbox(DaysSetting notification) => buildCheckbox(
+      notification: notification,
+      onClicked: () {
+        final newValue = !notification.value;
+
+        setState(() {
+          daysCompleted.value = newValue;
+          notifications.forEach((notification) {
+            notification.value = newValue;
+          });
+        });
+      });
+
+  Widget buildSingleCheckbox(DaysSetting notification) => buildCheckbox(
+    notification: notification,
+    onClicked: () {
+      setState(() {
+        final newValue = !notification.value;
+        notification.value = newValue;
+
+        if (!newValue) {
+          daysCompleted.value = false;
+        } else {
+          final allow =
+          notifications.every((notification) => notification.value);
+          daysCompleted.value = allow;
+        }
+      });
+    },
+  );
+
+  Widget buildCheckbox({
+    @required DaysSetting notification,
+    @required VoidCallback onClicked,
+  }) =>
+      ListTile(
+        onTap: onClicked,
+        leading: Checkbox(
+          value: notification.value,
+          onChanged: (value) => onClicked(),
+        ),
+        title: Text(
+          notification.title,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      );
+  void initState() {
+    super.initState();
+    _loadCounter();
+    _incrementCounter();
+    _savedCounter();
+  }
+  int _counter = 0;
+  _loadCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0);
+    });
+  }
+  _incrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _counter);
+    });
+  }
+  _savedCounter() async{
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _counter);
+    });
+  }
+}
